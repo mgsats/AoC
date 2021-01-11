6799dd80527be937c7c09b787bc41368866d7768
Michael Saterson <saterson@gmail.com>
2021-01-11 17:00:38 -0500

    refactor day 3 part 2 solution

--- problems/3/part_2.go
+++ problems/3/part_2.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"strings"
 )
 
 func main() {
+	slopeFile, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	slope := strings.Split(string(slopeFile), "\n")
+
 	slopeArgs := [][2]int{
 		{1, 1},
 		{3, 1},
@@ -15,41 +21,24 @@ func main() {
 		{7, 1},
 		{1, 2},
 	}
+
 	for _, v := range slopeArgs {
 		fmt.Printf(
 			"Number of trees encountered for slope Right %d, Down %d: %d\n", v[0], v[1],
-			slopeTreeCounter(v[0], v[1]),
+			slopeTreeCounter(slope, v[0], v[1]),
 		)
 	}
 }
 
-func slopeTreeCounter(xJump int, yJump int) int {
-	file, err := os.Open("input")
-	if err != nil {
-		fmt.Println("Unable to open input file")
-		os.Exit(1)
-	}
-	defer file.Close()
+func slopeTreeCounter(slope []string, xJump int, yJump int) int {
+	var treeCount, positionX, positionY int
 
-	scanner := bufio.NewScanner(file)
-	var treeCount int
-	position := -xJump
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		position = (position + xJump) % len(line)
-
-		if string(line[position]) == "#" {
+	for positionY < len(slope) && positionX < len(slope[positionY]) {
+		if string(slope[positionY][positionX]) == "#" {
 			treeCount++
 		}
-
-		for i := 1; i < yJump; i++ {
-			scanner.Scan()
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		positionX = (positionX + xJump) % len(slope[positionY])
+		positionY += yJump
 	}
 
 	return treeCount
